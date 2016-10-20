c449274a9470fdfc96e0b9c2a2d390f7739d1ced
AdamNagao <nagaoadam223@gmail.com>
2016-10-20 16:07:21 -0700

    Added basic player movement
    Added the basic player movement (no jump/no gravity), and made camera
    follow player

---
+++ LegendOf490/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float forwardSpeed = 12.0f;
+    public float turnSpeed = 150.0f;
+    //this runs once 
+    void Start()
+    {
+
+    }
+
+    //this runs every frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * forwardSpeed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
+
