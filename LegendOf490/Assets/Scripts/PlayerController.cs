6e4105d16767001f1c0e2b8ff901761880db288c
AdamNagao <nagaoadam223@gmail.com>
2016-10-20 18:22:20 -0700

    added player forward and attack animations

--- LegendOf490/Assets/Scripts/PlayerController.cs
+++ LegendOf490/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     public float forwardSpeed = 12.0f;
     public float turnSpeed = 150.0f;
+    
+ 
     //this runs once 
     void Start()
     {
@@ -16,11 +16,27 @@ public class PlayerController : MonoBehaviour
     //this runs every frame
     void Update()
     {
+        bool forwardPressed = Input.GetKeyDown(KeyCode.W);
+        bool attackPressed = Input.GetMouseButtonDown(0);
+
+        if (forwardPressed)
+        {
+            GameObject.Find("Player").GetComponent<Animation>().Play("Walk");
+        } else if (attackPressed)
+        {
+            GameObject.Find("Player").GetComponent<Animation>().Play("Attack");
+        }
+        else {
+            //play wait animation
+
+        }
+
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * forwardSpeed;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
     }
+
 }
 
