79608c4f3488f748cf87b3707de91b0d6e81826f
AdamNagao <nagaoadam223@gmail.com>
2016-10-25 20:30:20 -0700

    Camera movement smoothed, and camera returns to default position. Camera Angle needs to be adjusted

--- LegendOf490/Assets/Scripts/CameraController.cs
+++ LegendOf490/Assets/Scripts/CameraController.cs
@@ -3,38 +3,34 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    //
-    // VARIABLES
-    //
 
     public float turnSpeed = 4.0f;      // Speed of camera turning when mouse moves in along an axis
     public float zoomSpeed = 4.0f;
 
     private Vector3 mouseOrigin;    // Position of cursor when mouse dragging starts
     private Vector3 offset;
-    private float xAngle;
-    private float yAngle;
-    private float zAngle;
-    private float wAngle;
     private bool isRotating;    // Is the camera being rotated?
     private bool isZooming;        //are we zooming?
     private float scrollValue;
     public GameObject target;   //the player
+
     void Start()
     {
-        offset = transform.position - target.transform.position;
-        xAngle = Camera.main.transform.eulerAngles.x;
-        yAngle = Camera.main.transform.eulerAngles.y;
-        zAngle = Camera.main.transform.eulerAngles.z;
-
-
-        print("xAngle " + xAngle + "yAngle " + yAngle + "zAngle " + zAngle);
+     
     }
 
     void Update()
     {
-    
-        scrollValue = Input.GetAxis("Mouse ScrollWheel"); 
+        bool forwardPressed = Input.GetKeyDown(KeyCode.W);
+        isRotating = Input.GetMouseButton(1);
+        scrollValue = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollValue != 0)
+            isZooming = true;
+
+        // Disable movements on button release
+        if (scrollValue == 0)
+            isZooming = false;
 
         // Get the right mouse button
         if (Input.GetMouseButtonDown(1))
@@ -47,52 +43,83 @@ public class CameraController : MonoBehaviour
 		if (Input.GetKeyDown (KeyCode.R)) // reset camera when R is pressed
 		{
 			print ("r pressed");
-
-            Vector3 desiredPosition = target.transform.position + offset;
-            transform.position = desiredPosition;
-
-            Camera.main.transform.rotation = Quaternion.Euler(xAngle, yAngle, zAngle);
-
+         
         }
 
-        if(scrollValue != 0)
-            isZooming = true;
-        
-
-        // Disable movements on button release
-
-        if (!Input.GetMouseButton(1))
-            isRotating = false;
-
-        if(scrollValue == 0)
-            isZooming = false;
-        
-
+ 
         // Rotate camera along X and Y axis
         if (isRotating)
         {
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
-
-            transform.RotateAround(target.transform.position, transform.right, -pos.y * turnSpeed);
-            transform.RotateAround(target.transform.position, Vector3.up, pos.x * turnSpeed);
+            rotateAroundPlayer();
         }
         if (isZooming)
         {
-           
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
-
-            if (scrollValue > 0)
-            {
-                Vector3 move = pos.y * zoomSpeed * transform.forward;
-                transform.Translate(move, Space.World);
-            } else
-            {
-                Vector3 move = pos.y * zoomSpeed * transform.forward *-1 ;
-                transform.Translate(move, Space.World);
-            }
-           
+            zoomFromPlayer();  
         }
+        if ((!isRotating && !isZooming))
+        {
+            cameraFollowPlayer();
+        }
+       
+    }
 
+    public void rotateAroundPlayer()
+    {
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+        transform.RotateAround(target.transform.position, transform.right, -pos.y * turnSpeed);
+        transform.RotateAround(target.transform.position, Vector3.up, pos.x * turnSpeed);
         
+    }
+
+    public void zoomFromPlayer()
+    {
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+        if (scrollValue > 0)
+        {
+            Vector3 move = pos.y * zoomSpeed * transform.forward;
+            transform.Translate(move, Space.World);
+        }
+        else
+        {
+            Vector3 move = pos.y * zoomSpeed * transform.forward * -1;
+            transform.Translate(move, Space.World);
+        }
+    }
+
+    public void cameraFollowPlayer()
+    {
+        float distance = 15f;
+        // the height we want the camera to be above the target
+        float height = 20.0f;
+
+        float heightDamping = 2.0f;
+        float rotationDamping = 3.0f;
+
+        // Calculate the current rotation angles
+        float wantedRotationAngle = target.transform.eulerAngles.y;
+        float wantedHeight = target.transform.position.y + height;
+        float currentRotationAngle = transform.eulerAngles.y;
+        float currentHeight = transform.position.y;
+
+        // Damp the rotation around the y-axis
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+        // Convert the angle into a rotation
+        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        // Damp the height
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        // Set the position of the camera on the x-z plane to:
+        // distance meters behind the target
+        transform.position = target.transform.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        // Set the height of the camera
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+        // Always look at the target
+        Camera.main.transform.LookAt(target.transform);
     }
 }
