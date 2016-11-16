8d1b3cb2495d8d297cdfb9df9c82df92cc643b86
AdamNagao <nagaoadam223@gmail.com>
2016-11-15 21:08:50 -0800

    Redid Main Menu, and Test area terrain with new textures and then rendered in low poly. Also added new low poly assets

--- LegendOf490/Assets/Scripts/minimap.cs
+++ LegendOf490/Assets/Scripts/minimap.cs
@@ -1,24 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class MiniMap : MonoBehaviour {
 
-	// target will be the user selected in the inspector panel
-	public GameObject target = null;
-	// camheight is how high the camera is looking down from
-	public float camHeight = 200.0f;
+    // target will be the user selected in the inspector panel
+    public GameObject target = null;
+    // camheight is how high the camera is looking down from
+    public float camHeight = 200.0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update () {
-		// follow player on the x and z pos with a static height, no rotations
-		transform.position = new Vector3 (target.transform.position.x, camHeight, target.transform.position.z);
+    // Update is called once per frame
+    void Update()
+    {
+        // follow player on the x and z pos with a static height, no rotations
+        transform.position = new Vector3(target.transform.position.x, camHeight, target.transform.position.z);
 
 
-	}
+    }
 }
