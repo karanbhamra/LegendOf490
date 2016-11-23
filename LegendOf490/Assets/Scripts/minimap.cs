8fa265d3c2910fdd49a83c1dccc75a6fc969d8ec
AdamNagao <nagaoadam223@gmail.com>
2016-11-22 17:44:49 -0800

    Made shit smaller

--- LegendOf490/Assets/Scripts/minimap.cs
+++ LegendOf490/Assets/Scripts/minimap.cs
@@ -1,17 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MiniMap : MonoBehaviour {
+public class MiniMap : MonoBehaviour
+{
 
     // target will be the user selected in the inspector panel
-	public GameObject target;// = GameObject.FindGameObjectWithTag ("user");
+    public GameObject target;// = GameObject.FindGameObjectWithTag ("user");
     // camheight is how high the camera is looking down from
     public float camHeight = 200.0f;
 
     // Use this for initialization
     void Start()
     {
-		target = GameObject.FindGameObjectWithTag ("user");
+       
 
     }
 
