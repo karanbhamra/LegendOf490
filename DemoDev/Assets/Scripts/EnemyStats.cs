ca41b3eb8b47ce7e24d577bf8141751377bce365
Karan Bhamra <karan.bhamra@gmail.com>
2016-12-06 09:55:44 -0800

    Added testing trimmed down files

---
+++ DemoDev/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+public class EnemyStats : MonoBehaviour {
+
+  public float curHp;
+  public float maxHp;
+
+  // Use this for initialization
+  void Start() {
+    curHp = 1000;
+    maxHp = 1000;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (curHp <= 0) {
+      GameObject.Destroy(gameObject);
+    }
+  }
+
+  public void ReceiveDamage(float dmg) {
+    curHp -= dmg;
+
+    print("damage done = " + dmg);
+    print("enemy hp = " + curHp);
+  }
+}
