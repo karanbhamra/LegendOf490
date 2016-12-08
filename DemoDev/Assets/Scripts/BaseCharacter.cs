c50a90c213f51782045096d1dd9b541fc9453620
ToMa207 <macmarcus720@gmail.com>
2016-12-07 19:26:47 -0800

    Game Master

---
+++ DemoDev/Assets/Scripts/BaseCharacter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacter : MonoBehaviour {
+  private string name;
+  private string description;
+  private float health;
+  private float stamina;
+
+  public string NAME {
+    get { return this.name; }
+    set { this.name = value; }
+  }
+  public string DESCRIPTION {
+    get { return this.description; }
+    set { this.description = value; }
+  }
+  public float HEALTH {
+    get { return this.health; }
+    set { this.health = value; }
+  }
+  public float STAMINA {
+    get { return this.stamina; }
+    set { this.stamina = value; }
+  }
+  // Use this for initialization
+  void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DemoDev/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  public void StartGame() {
+    SceneManager.LoadScene("Prologue");
+  }
+}
