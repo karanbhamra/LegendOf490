41a142d7b2c8fc24b8f9b37352cb937111673759
ToMa207 <macmarcus720@gmail.com>
2016-12-08 07:07:17 -0800

    Fixed Movement

--- DemoDev/Assets/Scripts/CharacterController.cs
+++ DemoDev/Assets/Scripts/CharacterController.cs
@@ -6,9 +6,17 @@ public class CharacterController : MonoBehaviour {
   public Animator animator;
   private Vector3 moveDirection;
 
-  public float speed = 35.0f;
+  public float speed = 0.0f;
   public float h = 0.0f;
   public float v = 0.0f;
+  public float tiltAngleY;
+
+  bool upPressed;
+  bool downPressed;
+  bool rightPressed;
+  bool leftPressed;
+  bool attackPressed;
+  KeyCode currentStroke;
 
   public bool attack1 = false;  // used for attack mode 1
   public bool attack2 = false;  // used for attack mode 2
@@ -24,23 +32,54 @@ public class CharacterController : MonoBehaviour {
   }
 
   // Update is called once per frame
-
-
   void Update() {
-
+    if (speed > 0.05f) {
+      speed -= 0.05f;
+    }
+    if(speed > 35.0f) {
+      speed = 35.0f;
+    }
+    if(speed <= 0.05f){
+      speed = 0.0f;
+    }
   }
 
   void FixedUpdate() {
-    // The Inputs are defined in the Input Manager
-    h = Input.GetAxis("Horizontal") * -1.0f; // get value for horizontal axis
-    v = Input.GetAxis("Vertical") * -1.0f;   // get value for vertical axis
+    if(upPressed = Input.GetKey(KeyCode.W)) {
+      v = 1.0f;
+      tiltAngleY = 0.0f;
+    }
+    if (downPressed = Input.GetKey(KeyCode.S)) {
+      v = -1.0f;
+      tiltAngleY = 180.0f;
+    }
+    if(rightPressed = Input.GetKey(KeyCode.D)) {
+      h = 1.0f;
+      tiltAngleY = 90.0f;
+    }
+    if(leftPressed = Input.GetKey(KeyCode.A)) {
+      h = -1.0f;
+      tiltAngleY = 270.0f;
+    }
+    if(attackPressed = Input.GetMouseButton(0)) {
+      speed -= 0.1f;
+    }
 
-    transform.Translate(new Vector3(h * speed * Time.deltaTime, 0.0f, v * speed * Time.deltaTime));
+    // The Inputs are defined in the Input Manager
+    if(h != 0.0f || v != 0.0f && speed < 35.0f) {
+      speed += 0.2f;
+    }
+
+    transform.rotation = Quaternion.Euler(0.0f, tiltAngleY, 0.0f);
+    transform.position += transform.forward * Time.deltaTime * speed;
 
     animator.SetFloat("Speed", speed);
     animator.SetFloat("Horizontal", h);
     animator.SetFloat("Vertical", v);
+    animator.SetBool("MeleeAttack", attackPressed);
 
+    h = 0.0f;
+    v = 0.0f;
   }
 
 }
