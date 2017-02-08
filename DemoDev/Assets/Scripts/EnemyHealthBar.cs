245400fd9abf42e8022267835d3bdd5315e66cba
Karan Bhamra <karan.bhamra@gmail.com>
2017-02-08 13:48:03 -0800

    Enemy Health Bar now always faces camera, added that script to the Prefab model as well

---
+++ DemoDev/Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBar : MonoBehaviour {
+
+	private Camera cameraToLookAt;
+
+	// Use this for initialization
+	void Start () {
+		cameraToLookAt = Camera.main;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 v = cameraToLookAt.transform.position - transform.position;
+      	v.x = v.z = 0.0f;
+      	transform.LookAt( cameraToLookAt.transform.position - v ); 
+      	transform.Rotate(0,180,0);
+		
+	}
+}
